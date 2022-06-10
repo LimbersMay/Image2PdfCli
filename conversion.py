94567c5113c1541400a7f40bb368923dbe1965ed
limbersMay <limbermay1@gmail.com>
2022-06-10 17:40:55 -0500

    Se agregó el método para convertir un único archivo de png a jpg y un único archivo de jpg a pdf

--- conversion.py
+++ conversion.py
@@ -2,6 +2,7 @@ import os
 import img2pdf
 from io import open
 from PIL import Image
+from pathlib import Path
 
 class PngToJpg:
     def __init__(self, ruta_origen: str, ruta_destino: str, extension: str = "jpg") -> None:
@@ -25,6 +26,22 @@ class PngToJpg:
             img.save(self.ruta_origen + convertir_jpg[contador])
             os.remove(self.ruta_origen + listado_pngs[contador])
 
+    def realizar_conversion_unica(self):
+        # Obtenemos el nombre final de la ruta (nombre del fichero) y le cambiamos la extensión
+        nombre_archivo = Path(self.ruta_origen).name
+        convertir_jpg = nombre_archivo[:-3] + self.extension
+
+        img = Image.open(self.ruta_origen)
+
+        if img.mode in ("RGBA", "P"):
+            img = img.convert("RGB")
+
+        # Ruta final en donde se cambiará la extensión de Png a Jpg
+        self.ruta_origen = os.path.dirname(self.ruta_origen) + "/" + convertir_jpg
+
+        # Guardamos la imagen en Jpg en la ruta de origen y eliminamos la original en png
+        img.save(self.ruta_origen)
+        os.remove(self.ruta_origen)
 
 class ImgToPdf:
     def __init__(self, ruta_origen: str, ruta_destino: str, nombre: str = "unido.pdf") -> None:
@@ -37,6 +54,7 @@ class ImgToPdf:
         self.nombres_pdf: list[str]
     
     def obtener_informacion(self):
+
         self.listado_origen = os.listdir(self.ruta_origen)
         self.nombres_pdf = [archivo[:-3] + "pdf" if archivo.endswith(".jpg") else archivo[:-5] + ".pdf" for archivo in self.listado_origen]
     
@@ -46,6 +64,18 @@ class ImgToPdf:
         for contador in range(len(self.listado_origen)):
             with open(self.ruta_destino + self.nombres_pdf[contador], "wb") as fichero:
                 fichero.write(img2pdf.convert(self.ruta_origen + self.listado_origen[contador]))
+
+    def conversion_unico(self):
+        nombre_fichero = Path(self.ruta_origen).name
+
+        nombre_pdf = nombre_fichero[:-3] + ".pdf" if nombre_fichero.endswith(".jpg") else nombre_fichero[:-5] + ".pdf"
+
+        # Eliminamos el nombre del fichero del Path
+        self.ruta_origen = os.path.dirname(self.ruta_origen) + "/"
+        self.ruta_destino = self.ruta_destino + "/"
+
+        with open(self.ruta_destino + nombre_pdf, "wb") as fichero:
+            fichero.write(img2pdf.convert(self.ruta_origen + nombre_fichero))
 
     def convertir_unido(self):
         self.obtener_informacion()
